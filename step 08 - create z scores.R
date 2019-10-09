3ebe6a6d631c69b8088d87bd5f6a25fe8e562bac
Austin Spence <32775738+austinspence@users.noreply.github.com>
2019-10-09 10:58:12 -0400

    added the post_breeding zscore calculations

--- step 08 - create z scores.R
+++ step 08 - create z scores.R
@@ -8,7 +8,7 @@
 #setwd("/Users/austinspence/Dropbox/BBS_Project")
 # Load in the full data set
 
-load("./BBS_Data/breeding_and_month_weather.RData")
+load("./BBS_Data/breed_postbreed_month_weather.Rdata")
 
 
 # A function that takes a column name, a list of species for which z-scores are desired,
@@ -35,18 +35,41 @@ zsc <- function(lag_weather, column_name, z_score_species){
 
 
 
-# Create the z_scores for desired columns across all the sites (gives you if it is in warm or cold part of range)
-full_birds <- zsc(lag_weather = breeding_and_month_weather, column_name = c("breeding_mean_high"), z_score_species = breeding_and_month_weather) # where is it at in the species breedingthermal range?
-full_birds <- zsc(lag_weather = full_birds, column_name = c("breeding_mean_low"), z_score_species = breeding_and_month_weather) # where is it at in the species thermal range?
-full_birds <- zsc(lag_weather = full_birds, column_name = c("breeding_mean_precip"), z_score_species = breeding_and_month_weather) # where is it at in the species precipitation range?
-full_birds <- zsc(lag_weather = full_birds, column_name = c("breeding_mean_swe"), z_score_species = breeding_and_month_weather) # where is it at in the species snow range?
-full_birds <- zsc(lag_weather = full_birds, column_name = c("mean_high_jul_aug"), z_score_species = breeding_and_month_weather) # where is it at in the sppecies jul/aug thermal range?
-full_birds <- zsc(lag_weather = full_birds, column_name = c("mean_precip_jul_aug"), z_score_species = breeding_and_month_weather) # where is it at in the sppecies jul/aug thermal range?
+#### Create the z_scores for desired columns across all the sites (gives you if it is in warm or cold part of range)
+# Breeding weather
+full_birds <- zsc(lag_weather = breed_postbreed_month_weather, column_name = c("breeding_mean_high"), z_score_species = breed_postbreed_month_weather)  # where is it at in the species breeding high thermal range?
+full_birds <- zsc(lag_weather = full_birds, column_name = c("breeding_mean_low"), z_score_species = breed_postbreed_month_weather)                   # where is it at in the species breeding low thermal range?
+full_birds <- zsc(lag_weather = full_birds, column_name = c("breeding_mean_precip"), z_score_species = breed_postbreed_month_weather)                # where is it at in the species breeding precipitation range?
+full_birds <- zsc(lag_weather = full_birds, column_name = c("breeding_mean_swe"), z_score_species = breed_postbreed_month_weather)                   # where is it at in the species breeding snow range?
 
-# Create the z_scores manually to see if it was an anomolous temperatre/precip that year
+# Post-breeding weather
+full_birds <- zsc(lag_weather = full_birds, column_name = c("post_breeding_mean_high"), z_score_species = breed_postbreed_month_weather)                  # where is it at in the species post-breeding high thermal range?
+full_birds <- zsc(lag_weather = full_birds, column_name = c("post_breeding_mean_low"), z_score_species = breed_postbreed_month_weather)                   # where is it at in the species post-breeding low thermal range?
+full_birds <- zsc(lag_weather = full_birds, column_name = c("post_breeding_mean_precip"), z_score_species = breed_postbreed_month_weather)                # where is it at in the species post-breeding precipitation range?
+full_birds <- zsc(lag_weather = full_birds, column_name = c("post_breeding_mean_swe"), z_score_species = breed_postbreed_month_weather)                   # where is it at in the species post-breeding snow range?
+
+# July/August weather
+full_birds <- zsc(lag_weather = full_birds, column_name = c("mean_high_jul_aug"), z_score_species = breed_postbreed_month_weather)                   # where is it at in the species jul/aug high thermal range?
+full_birds <- zsc(lag_weather = full_birds, column_name = c("mean_precip_jul_aug"), z_score_species = breed_postbreed_month_weather)                 # where is it at in the species jul/aug low thermal range?
+
+
+
+
+
+#### Create the z_scores manually to see if it was an anomolous temperatre/precip that year
+# Breeding weather
 full_birds$breeding_avg_high_year_1_zscore <- (full_birds$breeding_avg_high_year_1 - full_birds$breeding_mean_high)/full_birds$breeding_sd_high # was this specific year an anomoly for mean high temp?
 full_birds$breeding_avg_low_year_1_zscore <- (full_birds$breeding_avg_low_year_1 - full_birds$breeding_mean_low)/full_birds$breeding_sd_low # was this specific year an anomoly for mean low temp?
 full_birds$breeding_avg_precip_year_1_zscore <- (full_birds$breeding_avg_precip_year_1 - full_birds$breeding_mean_precip)/full_birds$breeding_sd_precip # was this specific year an anomoly for mean precip?
+full_birds$breeding_avg_swe_year_1_zscore <- (full_birds$breeding_avg_swe_year_1 - full_birds$breeding_mean_swe)/full_birds$breeding_sd_swe # was this specific year an anomoly for mean precip?
+
+# Post-breeding weather
+full_birds$post_breeding_avg_high_year_1_zscore <- (full_birds$post_breeding_avg_high_year_1 - full_birds$post_breeding_mean_high)/full_birds$post_breeding_sd_high # was this specific year an anomoly for mean high temp?
+full_birds$post_breeding_avg_low_year_1_zscore <- (full_birds$post_breeding_avg_low_year_1 - full_birds$post_breeding_mean_low)/full_birds$post_breeding_sd_low # was this specific year an anomoly for mean low temp?
+full_birds$post_breeding_avg_precip_year_1_zscore <- (full_birds$post_breeding_avg_precip_year_1 - full_birds$post_breeding_mean_precip)/full_birds$post_breeding_sd_precip # was this specific year an anomoly for mean precip?
+full_birds$post_breeding_avg_swe_year_1_zscore <- (full_birds$post_breeding_avg_swe_year_1 - full_birds$post_breeding_mean_swe)/full_birds$post_breeding_sd_swe # was this specific year an anomoly for mean precip?
+
+# July/August weather
 full_birds$monthly_avg_high_13_year_1_zscore <- (full_birds$monthly_avg_high_13_year_1 - full_birds$mean_high_jul_aug)/full_birds$sd_high_jul_aug #was this specific year an anomoly for jul/aug high temp?
 full_birds$monthly_avg_precip_13_year_1_zscore <- (full_birds$monthly_avg_precip_13_year_1 - full_birds$mean_precip_jul_aug)/full_birds$sd_precip_jul_aug #was this specific year an anomoly for jul/aug high precip?
 
